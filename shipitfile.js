c7aa26950167836524602382d7c5389fb007445a
Chris Hegre <hegrec@gmail.com>
2015-10-05 08:19:05 -0700

    Ship It Deployable

---
+++ shipitfile.js
@@ -0,0 +1,28 @@
+module.exports = function (shipit) {
+    require('shipit-deploy')(shipit);
+
+    shipit.initConfig({
+        default: {
+            workspace: '/tmp/browgine',
+            deployTo: '/opt/browgine',
+            repositoryUrl: 'https://github.com/hegrec/browgine.git',
+            ignores: ['.git', 'node_modules'],
+            keepReleases: 3
+        },
+        staging: {
+            servers: 'mealtrap.com'
+        },
+        production: {
+            servers: 'nodeapps@mealtrap.com'
+        }
+    });
+
+    shipit.on('published', function() {
+        shipit.remote('cd /opt/browgine/current && npm install').then(function(res) {
+            shipit.log(res);
+                shipit.remote('pm2 restart api').then(function(res) {
+                    shipit.log(res);
+                });
+        });
+    })
+};
